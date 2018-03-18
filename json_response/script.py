7125c3ab23c7f3d15e7d56599c3f0b3f792e83e2
Yagyansh Bhatia <yagyansh1999@gmail.com>
2018-03-19 03:17:42 +0530

    pluginclass

---
+++ json_response/script.py
@@ -0,0 +1,17 @@
+url = "https://github.com/yagyanshbhatia/Code_snippets/raw/master/json_response/manifest.json"
+import urllib.request
+
+data = urllib.request.urlopen(url) # read only 20 000 chars
+
+s = str(data.read())[2:-1]
+
+
+# # import urllib
+# # txt = urllib.urlopen(url).read()
+
+
+import json
+
+
+json_data = json.loads(s)
+print(json_data)
