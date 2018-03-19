c9029e86074ddc4e06de5a257ea3c4af7d5bc47b
Yagyansh Bhatia <yagyansh1999@gmail.com>
2018-03-19 19:38:59 +0530

    Update script.py

--- json_response/script.py
+++ json_response/script.py
@@ -1,12 +1,10 @@
-url = "https://github.com/yagyanshbhatia/Code_snippets/raw/master/json_response/manifest.json"
 import urllib.request
-
-data = urllib.request.urlopen(url) # read only 20 000 chars
-
-s = str(data.read())[2:-1]
-
 import json
 
+url = "https://github.com/yagyanshbhatia/Code_snippets/raw/master/json_response/manifest.json"
+
+data = urllib.request.urlopen(url) # read only 20 000 chars
+s = str(data.read())[2:-1]
 
 json_data = json.loads(s)
 print(json_data)
